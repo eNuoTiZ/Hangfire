2135522b776b8822e365246bf6636229aa96b40c
Unknown <o.guitton@gmail.com>
2018-07-20 11:35:28 -0400

    Adding solution

---
+++ TutoHangfire/Controllers/BuildMinesweeperController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TutoHangfire.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/BuildMinesweeper")]
+    public class BuildMinesweeperController : Controller
+    {
+        // GET: api/BuildMinesweeper
+        [HttpGet]
+        public void Get()
+        {
+            BackgroundJob.Enqueue(() =>
+                BuildMinesweeper()
+            );
+        }
+
+        // GET: api/BuildMinesweeper/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/BuildMinesweeper
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/BuildMinesweeper/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public int BuildMinesweeper()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = @"C:\Program Files\Unity\Editor\Unity.exe";
+            p.StartInfo.Arguments = @"-batchmode -projectPath=D:\Prog\Unity\Minesweeper -quit -batchmode -executeMethod BuildMyGame.BuildAndroid -logFile %LOCALAPPDATA%\Unity\Editor\Minesweeper.log D:\Prog\Unity\Minesweeper\Minesweeper.apk";
+
+            p.Start();
+            p.WaitForExit();
+            
+            return p.ExitCode;
+        }
+    }
+}
---
+++ TutoHangfire/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Hangfire.Dashboard;
+
+namespace TutoHangfire
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add Hangfire services.  
+            services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("LocalServer")));
+
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseBrowserLink();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
+
+            //app.UseStaticFiles();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseHangfireDashboard("");
+            app.UseHangfireServer();
+            //app.UseHangfireDashboard("/dashboard");
+
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseHangfireDashboard("", new DashboardOptions()
+            {
+                Authorization = new[] { new CustomAuthorizeFilter() }
+            });
+
+            //BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget Job Executed"));
+
+        }
+        
+    }
+
+    public class CustomAuthorizeFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpcontext = context.GetHttpContext();
+            return httpcontext.User.Identity.IsAuthenticated;
+        }
+    }
+
+}
